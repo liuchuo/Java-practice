66e08f9851f9f3da807eafa8e3a40c07eb38adee
陈欣 <95323362@qq.com>
2016-11-08 10:23:55 +0800

    0

---
+++ test2.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+import javax.swing.JApplet;
+public class test2 extends JApplet {
+    public void paint(Graphics g) {
+        g.drawString("this is a applet for java", 50, 50);
+    }
+}
---
+++ test3.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class test3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+        String s = buf.readLine();
+        System.out.println(s);
+    }
+}
