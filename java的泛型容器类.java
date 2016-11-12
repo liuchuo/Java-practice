efffa244c2d1db166450e728c39c79062aaba522
陈欣 <95323362@qq.com>
2016-11-12 08:59:14 +0800

    泛型和容器类

--- java的泛型容器类.java
+++ java的泛型容器类.java
@@ -25,3 +25,72 @@ public class Exercise {
 }
 
 
+// 集合 A = {1, 2, 3, 4} 和 B = {1， 3， 5， 7， 9， 11} ,变成求A与B的交集、并集、差集
+import java.util.HashSet;
+import java.util.Set;
+
+public class Exercise {
+    public static void main(String[] args) {
+        HashSet<Integer> a = new HashSet<>();
+        HashSet<Integer> b = new HashSet<>();
+        for(int i = 1; i <= 4; i++) {
+            a.add(i);
+        }
+        for(int i = 1; i <= 11; i += 2) {
+            b.add(i);
+        }
+
+        HashSet<Integer> temp = new HashSet<>(a);
+        temp.retainAll(b);
+        for(Integer i : temp) {
+            System.out.print(i + " ");
+        }
+
+        temp = a.clone();
+        temp.addAll(b);
+        Iterator<Integer> it = temp.iterator();
+        while(it.hasNext()) {
+            System.out.print(it.next() + " ");
+        }
+
+        temp = a.clone();
+        temp.remove(b);
+        Iterator<Integer> itt = temp.iterator();
+        while(it.hasNext()) {
+            System.out.print(it.next() + " ");
+        }
+    }
+}
+
+
+// 利用随机函数生成10个随机数，将他们有序地存入到一个TreeSet对象中，然后利用迭代器有序地输出
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class Exercise {
+    public static void main(String[] args) {
+        TreeSet<Double> treeSet = new TreeSet<>();
+        for(int i = 0; i < 10; i++) {
+            treeSet.add(Math.random());
+        }
+        Iterator<Double> it = treeSet.iterator();
+        while(it.hasNext()) {
+            System.out.print(it.next() + " ");
+        }
+    }
+}
+
+
+// 利用HashMap类对象存储公司电话号码簿，其中包含公司的电话号码和公司名称，然后删除一个公司和查询一个公司的操作。
+import java.util.HashMap;
+
+public class Exercise {
+    public static void main(String[] args) {
+        HashMap<String, String> number = new HashMap<>();
+        number.put("A", "1111111");
+        number.put("B", "2222222");
+        number.put("C", "3333333");
+        number.remove("A");
+        number.containKey("C");
+    }
+}
